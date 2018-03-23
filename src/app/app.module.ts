4d7ff0b9c64027fa5ccdd70fec73564cf903fb38
Lino Catucci <LinoCatucci@gmail.com>
2018-03-23 14:22:07 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { WeatherItemComponent } from './weather-item/weather-item.component';
+import { WeatherListComponent } from './weather-list/weather-list.component';
+
+
+@NgModule({
+  declarations: [AppComponent, WeatherItemComponent, WeatherListComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
