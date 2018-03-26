775aed058d50950e3bb9bc20544baa1bb4ac38b6
Lino Catucci <LinoCatucci@gmail.com>
2018-03-26 16:18:53 +0200

    small changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { WeatherItemComponent } from './weather-item/weather-item.component';
-import { WeatherListComponent } from './weather-list/weather-list.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { WeatherItemComponent } from "./weather-item/weather-item.component";
+import { WeatherListComponent } from "./weather-list/weather-list.component";
 
 @NgModule({
   declarations: [AppComponent, WeatherItemComponent, WeatherListComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    ReactiveFormsModule],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/weather-item/weather-item.component.ts
+++ src/app/weather-item/weather-item.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { WeatherItem } from '../models/weather.item.model';
+import { Component, OnInit } from "@angular/core";
+import { WeatherItem } from "../models/weather.item.model";
 
 @Component({
-  selector: 'app-weather-item',
-  templateUrl: './weather-item.component.html',
-  styleUrls: ['./weather-item.component.css']
+  selector: "app-weather-item",
+  templateUrl: "./weather-item.component.html",
+  styleUrls: ["./weather-item.component.css"]
 })
 export class WeatherItemComponent implements OnInit {
   weatherItem: WeatherItem;
 
   constructor() {
-    this.weatherItem = new WeatherItem('London', 'Rainy', 10);
+    this.weatherItem = new WeatherItem("London", "Rainy", 10);
   }
 
   ngOnInit() {}
